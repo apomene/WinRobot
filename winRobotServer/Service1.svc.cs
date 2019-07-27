90873ae6ab5d931c57837a5abffa20a8b2e524a5
Apo Mene <apomene@yahoo.gr>
2019-07-27 15:33:45 +0300

    First Succesfull Host in WPF

--- winRobotServer/Service1.svc.cs
+++ winRobotServer/Service1.svc.cs
@@ -6,28 +6,17 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace winRobotServer
+namespace winRobotService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public void SimpleAction()
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            IContractActions callback = OperationContext.Current.GetCallbackChannel<IContractActions>();
+            callback.CallBackAction("Calling from Call Back");
         }
     }
+
 }
