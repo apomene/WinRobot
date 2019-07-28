cb92a2d53e2ee986703c5e454306b2fb85bfb0f9
Apo Mene <apomene@yahoo.gr>
2019-07-28 19:52:37 +0300

    Added Robot CLient Application

---
+++ Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RobotActions.RobotClient robot = new RobotActions.RobotClient();
+            var test = robot.GetData(3);
+            Console.WriteLine(test);
+            Console.Read();
+        }
+    }
+}
