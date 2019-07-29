8fad372c235a8eda791a7648b1638633d9724cef
Apo Mene <apomene@yahoo.gr>
2019-07-29 22:40:21 +0300

    Added UI controls for all actions plus the whole Action Script on WPF project:

--- WpfRobotServer/MainWindow.xaml.cs
+++ WpfRobotServer/MainWindow.xaml.cs
@@ -28,53 +28,12 @@ namespace WpfRobotServer
         {
             InitializeComponent();
             InitControls();
-           // StartRobot();
         }
 
         private void InitControls()
         {
-            KeysList.ItemsSource = new List<string>() { "A","B","Hello WOrld!!","{ENTER}" };
+            ClickList.ItemsSource = new List<string>() { "Left Click","Right CLick","Double CLick" };
         }
-
-        private void StartRobot()
-        {
-            try
-            {
-                //using (
-                host = new ServiceHost(typeof(Robot));
-                //{
-                    //// Check to see if the service host already has a ServiceMetadataBehavior
-                    //ServiceMetadataBehavior smb = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
-                    //// If not, add one
-                    //if (smb == null)
-                    //    smb = new ServiceMetadataBehavior();
-                    //smb.HttpGetEnabled = true;
-                    //smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
-                    //host.Description.Behaviors.Add(smb);
-                    //// Add MEX endpoint
-                    //host.AddServiceEndpoint(
-                    //  ServiceMetadataBehavior.MexContractName,
-                    //  MetadataExchangeBindings.CreateMexHttpBinding(),
-                    //  "mex"
-                    //);
-                    host.AddServiceEndpoint(typeof(IRobot),
-                        new WSDualHttpBinding(), "http://localhost:8080/robotService/");
-
-                   // host.AddServiceEndpoint(typeof(IRobot),
-                    //    new WSHttpBinding(), "http://localhost:8080/robotService/wsAddress");
-
-                    //host.AddServiceEndpoint(typeof(IRobot),
-
-                    //    new NetTcpBinding(), "net.tcp://localhost:8081/RobotService");
-
-                    host.Open();
-                //}
-            }
-            catch (Exception ex)
-            {
-                host.Abort();
-                MessageBox.Show("Error = " + ex.Message);
-            }
-        }
+      
     }
 }
