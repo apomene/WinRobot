054a9632caf49b2391a0e59083e4c15f9e377de0
Apo Mene <apomene@yahoo.gr>
2019-07-29 22:40:21 +0300

    Added UI controls for all actions plus the whole Action Script on WPF project:

--- WpfRobotServer/ViewModel/ViewModelMain.cs
+++ WpfRobotServer/ViewModel/ViewModelMain.cs
@@ -12,6 +12,7 @@ namespace WpfRobotServer.ViewModel
 {
     class ViewModelMain:ViewModelBase
     {
+        #region UI proprties and fields
         string _textTitle;
         public string TextTitle
         {
@@ -29,23 +30,110 @@ namespace WpfRobotServer.ViewModel
             }
         }
 
-        public RelayCommand SetWindowByTitle { get; set; }
+        string _textKeys;
+        public string TextKeys
+        {
+            get
+            {
+                return _textKeys;
+            }
+            set
+            {
+                if (_textKeys != value)
+                {
+                    _textKeys = value;
+                    RaisePropertyChanged("TextKeys");
+                }
+            }
+        }
 
+        string _textMouseMove;
+        public string TextMouseMove
+        {
+            get
+            {
+                return _textMouseMove;
+            }
+            set
+            {
+                if (_textMouseMove != value)
+                {
+                    _textMouseMove = value;
+                    RaisePropertyChanged("TextMouseMove");
+                }
+            }
+        }
+        string _textActions;
+        public string TextActions
+        {
+            get
+            {
+                return _textActions;
+            }
+            set
+            {
+                if (_textActions != value)
+                {
+                    _textActions = value;
+                    RaisePropertyChanged("TextActions");
+                }
+            }
+        }
+        #endregion
+
+        #region Commands - Button Clicks
+        public RelayCommand SetWindowByTitle { get; set; }
+        public RelayCommand SetText { get; set; }
+        public RelayCommand MouseClick { get; set; }
+        public RelayCommand MouseMove { get; set; }
+        public RelayCommand ClearActions { get; set; }
+        public RelayCommand SendActions { get; set; }
+        #endregion
         public ViewModelMain()
         {
-            SetWindowByTitle = new RelayCommand(SetWindow);
+            SetWindowByTitle = new RelayCommand(ActionWindow);
+            SetText = new RelayCommand(ActionText);
+            MouseClick = new RelayCommand(ActionClick);
+            MouseMove = new RelayCommand(ActionMove);
+            ClearActions = new RelayCommand(Clear);
+            SendActions = new RelayCommand(Send);
         }
 
         RobotClient proxy;
-        private void SetWindow(object parameter)
+        private void ActionWindow(object parameter)
         {
             //The server sends the action or action script to the connected client. 
             // InstanceContext context = new InstanceContext(this);
             //proxy = new RobotClient(context);
-            RobotMethods robotActions = new RobotMethods();
+           // RobotMethods robotActions = new RobotMethods();
             string actionScript = $"{Actions.Select}:{TextTitle}";
-            robotActions.SendActionScript(actionScript);
-
+           // robotActions.SendActionScript(actionScript);
+            this.TextActions += actionScript + Environment.NewLine;
+        }
+        private void ActionText(object parameter)
+        {
+            string actionScript = $"{Actions.SendKeys}:{TextKeys}";
+            this.TextActions += actionScript + Environment.NewLine;
+        }
+        private void ActionClick(object parameter)
+        {
+            string actionScript = $"{Actions.Click}:{TextKeys}";
+            this.TextActions += actionScript + Environment.NewLine;
+        }
+        private void ActionMove(object parameter)
+        {
+            string actionScript = $"{Actions.Move}:{TextMouseMove}";
+            this.TextActions += actionScript + Environment.NewLine;
+        }
+        private void Clear(object parameters)
+        {
+            this.TextActions = "";
+        }
+        private void Send(object parameters)
+        {
+            //The server sends the action or action script to the connected client.     
+             RobotMethods robotActions = new RobotMethods();
+             robotActions.SendActionScript(TextActions);        
         }
     }
 }
