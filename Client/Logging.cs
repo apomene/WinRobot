bdbf1a146e07cd8c0dd62eb06a14870cf3f3a034
Apostolos Meneklis <ameneklis@hertz.gr>
2019-07-30 16:49:11 +0300

    Added NLog

---
+++ Client/Logging.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NLog;
+
+
+namespace Logging
+{
+   
+    public static class Logging
+    {
+        private static NLog.Logger _log = NLog.LogManager.GetLogger("generalLogger");
+      
+        public static void LogMsgToFile(string msg)
+        {
+            _log.Info(msg);
+        }
+
+        public static void LogErrorToFile(string msg)
+        {
+            _log.Error(msg);
+        }
+
+    }
+}
---
+++ Logger/Logging.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using NLog;
+
+
+namespace BcpShell
+{
+   
+    public static class Logging
+    {
+        private static NLog.Logger _log = NLog.LogManager.GetLogger("generalLogger");
+
+        private static NLog.Logger _metricLogger = NLog.LogManager.GetLogger("metricLogger");
+
+        public static string ConString { get; set; }
+        public static void LogMsgToFile(string msg)
+        {
+            _log.Info(msg);
+        }
+
+        public static void LogErrorToFile(string msg)
+        {
+            _log.Error(msg);
+        }
+
+        public static void LogToDatabase(string tblName, int srcRows,int trgRows)
+        {
+           //TO DO??
+        }
+
+        public static void LogMetrics(Dictionary<string,int> metrics,string databaseName)
+        {
+            _metricLogger.Info($"Metrics for Database: {databaseName} ...");
+            foreach (var metric in metrics)
+            {
+                _metricLogger.Info($"Table Name: {metric.Key} \t \t #Rows: {metric.Value}");
+            }
+        }
+
+        public static void LogMetrics(Dictionary<string, int> metrics)
+        {         
+            foreach (var metric in metrics)
+            {
+                _metricLogger.Info($"Table Name: {metric.Key} \t \t #Rows: {metric.Value}");
+            }
+        }
+
+        public static void LogMetrics()
+        {           
+           _metricLogger.Info(Environment.NewLine);          
+        }
+    }
+}
